3bf085c946bd29cc6f748bc51226500c844fb525
cq522138 <41883350+cq522138@users.noreply.github.com>
2018-12-13 11:20:16 +0700

    Add files via upload

---
+++ countdown-timer.component.ts
@@ -0,0 +1,65 @@
+import {Component, OnInit, Input, OnDestroy} from '@angular/core';
+
+@Component({
+  selector: 'app-countdown-timer',
+  templateUrl: './countdown-timer.component.html',
+  styleUrls: ['./countdown-timer.component.css']
+})
+export class CountdownTimerComponent implements OnInit {
+  private intervalId = 0;
+  message = '';
+  remainingTime: number;
+  @Input()
+  seconds = 11;
+
+  clearTimer() {
+    clearInterval(this.intervalId);
+  }
+
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.reset();
+    this.start();
+  }
+
+  // ngOnDestroy() {
+  //   this.clearTimer()
+  // }
+
+  start(){
+    this.countDown ();
+    if(this.remainingTime <=0){
+      this.remainingTime = this.seconds;
+    }
+  }
+
+  stop(){
+    this.clearTimer();
+    this.message = `holding at t-${this.remainingTime} seconds`
+  }
+
+  reset(){
+    this.clearTimer();
+    this.remainingTime = this.seconds;
+    this.message = `Click start button to start the Countdown`;
+  }
+
+
+  private countDown(){
+    this.clearTimer();
+    this.intervalId = window.setInterval(()=>{
+      this.remainingTime -=1;
+      if(this.remainingTime === 0){
+        this.message = 'Blast off!';
+        this.clearTimer();
+      }
+      else{
+        this.message = `T-${this.remainingTime} seconds and counting`;
+      }
+    },1000);
+  }
+
+}
